55e8204763695251515e875dc30c297cd545504c
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2022-03-04 12:41:37 +0900

    取得に成功できていたが、後程、The data couldn’t be read because it isn’t in the correct formatというエラーが表示された。 xcodeのバグかもしれないのでパソコンを再起動してみる。

---
+++ SearchSweetsApp/CustomeCell.swift
@@ -0,0 +1,15 @@
+//
+//  CoustomeCell.swift
+//  SearchSweetsApp
+//
+//  Created by 中野翔太 on 2022/03/04.
+//
+
+import UIKit
+class CustomCell: UITableViewCell {
+    @IBOutlet private weak var titleImage: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    func configure(titleImage: String, titleLabel: String) {
+        
+    }
+}
--- SearchSweetsApp/ViewController.swift
+++ SearchSweetsApp/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+// JSONの構造
+struct ResultJson: Codable {
+    let item: [ItmeJson]?
+}
+struct ItmeJson: Codable {
+    let name: String?
+    let maker: String?
+    let url: URL?
+    let image: URL?
+}
+var okashiList: [ResultJson] = []
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource {
 
+    @IBOutlet private weak var searchText: UISearchBar!
+    @IBOutlet private weak var tableView: UITableView!
+
+    // 画面表示の際に１回しか呼ばれない
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        if let searchWord = searchBar.text {
+            print(searchWord)
+            // 検索ボタンを押すと実行される
+            searchOkashi(keyword: searchWord)
+        }
+    }
+    func searchOkashi(keyword: String) {
+                                                // 取得した文字列をエンコードしている
+        guard let keywordEncode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        guard let requestUrl = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keywordEncode)&max=10&order=r") else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: requestUrl){ data, response, error in
+            if let error = error {
+                return print(error.localizedDescription)
+            }
+             // dataとresponseが取れなかったら早期リターン&オプショナルバインディングの役割
+        guard let response = response, let data = data else {
+                return
+            }
 
+            do {
+                let json = try JSONDecoder().decode(ResultJson.self, from: data)
+                okashiList.append(json)
+//                self.tableView.reloadData()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        task.resume()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        okashiList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath) as? CustomCell else {
+             fatalError()
+        }
+//        cell.configure(titleImage: , titleLabel: <#T##String#>)
+        return cell
+    }
 }
-
